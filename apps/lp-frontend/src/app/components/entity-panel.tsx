4dff4c54463c7838f7b4e176cabc736245968df0
Yona Appletree <hypher@gmail.com>
2024-12-28 10:44:38 -0800

    initial entity panel work

--- apps/lp-frontend/src/app/components/entity-panel.tsx
+++ apps/lp-frontend/src/app/components/entity-panel.tsx
@@ -1,11 +1,28 @@
 export function EntityPanel() {
   return (
-    <div className={'border font-sans'}>
-      <h1>Welcome to EntityPanel!</h1>
+    <div className={'border border-gray-500'}>
+      {/* ------------------------------------------------------------------ */}
+      {/* Header */}
 
-      <section>
-        <h2>Input</h2>
-      </section>
+      <h1
+        className={
+          'bg-gray-800 px-1 m-0 border-b border-gray-500 flex items-baseline'
+        }
+      >
+        <strong className={'flex-grow'}>LED Strip</strong>
+        <em className={'text-gray-400 text-sm'}>Sequential RGB Fixture</em>
+      </h1>
+
+      {/* ------------------------------------------------------------------ */}
+      {/* Content */}
+
+      <div className={'px-1'}>
+        <section>
+          <h2 className={'text-sm text-gray-500 border-b border-gray-500'}>
+            Input
+          </h2>
+        </section>
+      </div>
     </div>
   );
 }
